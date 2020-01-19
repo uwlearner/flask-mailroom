a13aeb5e87a049b19f4278564f2b23d2f6336f10
uwlearner <>
2020-01-19 14:23:13 -0800

    import Donor

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
