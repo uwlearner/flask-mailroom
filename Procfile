53786e0a5143e97fc60f8bb33b82170488e42d04
uwlearner <>
2020-01-25 12:35:31 -0800

    minor update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:6738 main:app
+web: gunicorn main:app
--- main.py
+++ main.py
@@ -35,4 +35,3 @@ def donate():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
